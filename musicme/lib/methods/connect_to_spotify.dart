2fb68578b41b633222eaa99a7e02be7d6c0f6424
erikd234 <erikd234@gmail.com>
2021-02-28 18:35:04 -0800

    Intergrating spotifySDK
    Set up and authenticated Spotify with the app so we can load songs and interact with the Spotify player.

---
+++ musicme/lib/methods/connect_to_spotify.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/services.dart';
+import 'package:spotify_sdk/spotify_sdk.dart';
+
+Future<void> connectToSpotify() async {
+  try {
+    await SpotifySdk.connectToSpotifyRemote(
+      clientId: '47a74f6401d343debec2c0f6634e0aeb',
+      redirectUrl: 'spotify-ios-quick-start://spotify-login-callback',
+    );
+    print("connected");
+  } on PlatformException catch (e) {
+    print(e.message);
+  } on MissingPluginException {
+    print("not implemented");
+  }
+}
