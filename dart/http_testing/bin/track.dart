9afc94fec8ec2f766845c0f14a6cc5a4dc48080c
Erik Dahl <erikd234@gmail.com>
2021-03-01 19:34:27 -0800

    Implementing TrackProvider
    Finished implementing trackProvider so now we can get data from our Firebase server based off a simple energy query

---
+++ dart/http_testing/bin/track.dart
@@ -0,0 +1,12 @@
+class Track {
+  final trackId;
+
+  Track({this.trackId});
+}
+
+class TrackQueryParams {
+  int major;
+  List<double> danceability; // low is 0 high is 1, just like binary.
+  List<double> energy;
+  List<double> acousticness;
+}
--- dart/http_testing/bin/trackDataProvider.dart
+++ dart/http_testing/bin/trackDataProvider.dart
@@ -1,4 +1,4 @@
-import 'dart:convert';
+import 'dart:convert' as convert;
 import 'dart:io';
 import 'dart:web_audio';
 import 'package:http/http.dart' as http;
@@ -7,92 +7,78 @@ import 'sampleEntityIbmData.dart';
 import 'dart:async';
 import 'dart:math';
 
+import 'track.dart';
+
 /* trackDataProvider.dart
 Request track from Firebase
 Input void
 Output track_ID
 */
 
-class TrackDataProvider extends MoodDataProvider {
-  Future<IbmData> readData() async {
-    //need to authenticate this from Firebase
-    var uname = '';
-    var pword = '';
-    var authn = 'Basic ' + base64Encode(utf8.encode('$uname:$pword'));
-    var sentence = 'I hate the world I hate you';
-    var params = {
-      'version': '2017-09-21',
-      'text': sentence,
-    };
-
-    var query = params.entries.map((p) => '${p.key}=${p.value}').join('&');
-    print(query);
-    var response = await http.get(
-      'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/a8625766-fb0f-46f1-baa3-4cca671d6961/v3/tone?$query?sentences=true',
-      headers: {'Authorization': authn},
-    );
-
-    var ibmData;
-    if (response.statusCode == 200) {
-      var ibmDataInJson = json.decode(response.body);
-      ibmData = IbmData.fromJson(ibmDataInJson);
-      print('Sucessfully got data from Ibm');
-    } else {
-      print(response.statusCode);
-      assert(response.statusCode == 200);
-    }
-    return ibmData;
+class TrackDataProvider {
+  String _getRandomMood() {
+    var moods = ['joy', 'anger', 'sad'];
+    return moods[Random().nextInt(3)];
   }
-}
+
+  TrackQueryParams _getQueryParams(var moodIn) {
+    var mood = moodIn;
+    var moods = ['joy', 'anger', 'sad'];
+    TrackQueryParams params = TrackQueryParams();
+
+    if (moods.contains(mood) == false) {
+      mood =
+          _getRandomMood(); // if the mood doesnt exist then this function will return a random mood to work with.
+    }
+    if (mood == 'joy') {
+      params.major = 1;
+      params.danceability = [0.5, 1];
+      params.energy = [0.6, 1];
+    } else if (mood == 'sadness') {
+      params.major = 0;
+      params.danceability = [0, 0.4];
+      params.energy = [0, 0.6];
+    } else if (mood == 'anger') {
+      params.acousticness = [0, 0.6];
+      params.energy = [0.6, 1];
+    } else {
+      params.major = 1;
+      params.danceability = [0.5, 1];
+      params.energy = [0.6, 1]; // default Params to joy
+    }
+  }
+
+  Future<Track> readData(String mood) async {
+    // ignore: omit_local_variable_types
+    TrackQueryParams params = _getQueryParams(mood);
+    // Query by energy!
+    var energyQueryParams = {
+      'orderBy': '"energy"',
+      'startAt': '${params.energy[0]}',
+      'endAt': '${params.energy[1]}',
+      'limitToLast': '10',
+      'print': 'pretty',
+    };
+    var query =
+        energyQueryParams.entries.map((p) => '${p.key}=${p.value}').join('&');
+
+    var response = await http.get(
+        'https://musicme-fd43b-default-rtdb.firebaseio.com/tracks.json?$query');
+    if (response.statusCode != 200)
+      throw Exception('http.get error: statusCode= ${response.statusCode}');
+    print(response.body);
+    var returnJSON =
+        convert.jsonDecode(response.body); //convert http response to JSON
+    var songKeyList = returnJSON.keys.toList();
+    var firstTrackKey = songKeyList[0];
+    var firstTrackID = returnJSON[firstTrackKey]['id'];
+
+    var track = Track(trackId: firstTrackID);
+
+    return track;
+  }
 
 // returns a random emotion
-String getRandomMood() {
-  var moods = ['joy', 'anger', 'sad'];
-  return moods[Random().nextInt(3)];
-}
 
-Future<void> main() async {
-  //random number between 0-2
-  var random = Random();
-  // ignore: unused_local_variable
-  var randomNumber = random.nextInt(3);
-  // ignore: unused_local_variable
-  var songKey, major, danceability, energy, acousticness;
-  var mood = 'joy';
-//if we get a mood from IBM, then print the mood
-  switch (mood) {
-    case 'joy':
-      {
-        songKey = {'C', 'D', 'A', 'BB'};
-        major = 1;
-        danceability = [0.5, 1];
-        energy = [0.6, 1];
-      }
-      break;
-
-    case 'sad':
-      {
-        songKey = {'C', 'C#', 'DB', 'D', 'D#', 'F', 'F#', 'BB', 'B'};
-        major = 0;
-        danceability = [0, 0.4];
-        energy = [0, 0.6];
-      }
-      break;
-
-    case 'anger':
-      {
-        songKey = {'C#', 'D', 'D#', 'EB', 'E', 'F', 'F#', 'G', 'AB', 'B'};
-        major = {0, 1};
-        acousticness = [0, 0.6];
-        energy = [0.6, 1];
-      }
-      break;
-
-    //else, give it a random mood
-    default:
-      {
-        getRandomMood(); // define above, just gets a random emotion.
-      }
-      break;
-  }
+  Future<void> main() async {}
 }
