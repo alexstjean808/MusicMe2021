9e02220bbe3d5de10816896623cab191c35d41cf
Adam Fong <adamfong888@gmail.com>
2021-03-04 23:13:24 -0800

    feeling_lucky_function
    Just call this function and it will return a random track id.

--- dart/feelingLucky/feeling_lucky/lib/feeling_lucky.dart
+++ dart/feelingLucky/feeling_lucky/lib/feeling_lucky.dart
@@ -1,3 +1,34 @@
-int calculate() {
-  return 6 * 7;
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+import 'dart:math';
+
+void main() async {
+  print(await feelingLucky());
+}
+
+Future<String> feelingLucky() async {
+  var range = 15000;
+  var randomNumber = Random().nextInt(range);
+
+  //parameters are key for the firebase and a random number between 0 and the length of the database that we hardcoded
+  //code taken mostly from https://curl.trillworks.com/#dart
+  var params = {
+    'orderBy': r'%22$key%22',
+    'equalTo': '%22' + randomNumber.toString() + '%22',
+  };
+  var query = params.entries.map((p) => '${p.key}=${p.value}').join('&');
+
+  var res = await http.get(
+      'https://musicme-fd43b-default-rtdb.firebaseio.com/tracks.json?$query');
+  if (res.statusCode != 200) {
+    throw Exception('http.get error: statusCode= ${res.statusCode}');
+  }
+  //end code from curl.trillworks.com
+
+  var returnJSON = convert.jsonDecode(res.body); //convert http response to JSON
+  var trueSongNumber = returnJSON.keys.toList()[0];
+
+  var randomSongId = returnJSON[trueSongNumber]['id'];
+
+  return randomSongId;
 }
--- dart/feelingLucky/feeling_lucky/test/feeling_lucky_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:feeling_lucky/feeling_lucky.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
